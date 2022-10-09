b7a196735a91d4b186ba7e8df69db8463418de33
pryan <pryan@EC2AMAZ-TALBF09.health.local>
2022-10-09 20:51:53 +0000

    adding set-preferred name

--- fhirValidate.sh
+++ fhirValidate.sh
@@ -10,5 +10,5 @@ mkdir ./temp
 echo validating Patient examples
 cp ./Patient-*.json ./temp
 java -jar /C/apps/FHIR_validator/validator_cli.jar ./temp  -ig ./package.tgz -profile http://hl7.org.nz/fhir/StructureDefinition/NhiPatient -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -html-output ./validation_NhiPatient.html
-rm ./*temp/*.json
+#rm ./*temp/*.json
 
