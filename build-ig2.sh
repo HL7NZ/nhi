36ed4042aebd04c74f23c9235e2037273639e9e8
Pat Ryan <pat.ryan@health.govt.nz>
2022-10-26 02:28:05 +0000

    build on linux

--- build-ig2.sh
+++ build-ig2.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
-# build from ide-pat-linux
+set -x #echo on
 
-# echo cleaning up temp directory ...
+echo cleaning up temp directory ...
 rm -r  ./temp
 
 echo running sushi ...
-./runSushi.sh
 
-rm full-ig.zip
+./localscripts/makeTerminologySummary.js
+
 echo getting dependencies...
 nzbase_url=$(yq '.dependencies."fhir.org.nz.ig.base".uri' ./sushi-config.yaml)
 nzbase_version=$(yq '.dependencies."fhir.org.nz.ig.base".version' ./sushi-config.yaml)
 
 echo nzbase url =$nzbase_url
 echo nzbase version =$nzbase_version
-wget -e use_proxy=yes -e https_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"/full-ig.zip"
+
 
 #cleanup from previous run
+rm ./full-ig.zip
 rm -r site
 rm -r package
+wget -e use_proxy=yes -e http_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"/full-ig.zip"
 unzip ./full-ig.zip site/package.tgz
-unzip ./full-ig.zip site/*.xml
 
 tar zxvf ./site/package.tgz
 #fix the package url:
-jq --arg url $nzbase_url '.url |= $url' ./package/package.json > package.tmp && mv package.tmp ./package/package.json
+jq --arg url $nzbase_url '.url |= $url' ./package/package.json
 
 
-#cp nz packages  into users .fhir cache
- cp -r ./package ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version
+#cp nz packages  into users .fhir cache 
+ cp -r ./package ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version/
 
-cp ./input/template/* $HOME/.fhir/packages/fhir.base.template#current/package/content
+
+
+echo running sushi ...
+
+./localscripts/makeTerminologySummary.js
+sushi -o .
+
+./localscripts/makeTerminologySummary.js
 
 echo running ig publisher
 java -jar /opt/fhir/publisher.jar -ig . -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -no-sushi
-# ./fhirValidate.sh
+
+./fhirValidate.sh
