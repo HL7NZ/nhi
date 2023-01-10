821fcc67724c4707aa10d5c0cf5ebb6aad4dc00e
pryan <pryan@EC2AMAZ-TALBF09.health.local>
2023-01-10 23:57:21 +0000

    get package direct from nzbase and use  env variable for cred

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -13,8 +13,8 @@ echo nzbase url =$nzbase_url
 echo nzbase version =$nzbase_version
 
 
-wget -e use_proxy=yes -e http_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"/full-ig.zip"
-unzip ./full-ig.zip site/package.tgz
+wget -e use_proxy=yes -e http_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"package.tgz"
+mv ./package.tgz site/package.tgz
 
 tar zxvf ./site/package.tgz
 #fix the package url:
