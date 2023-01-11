1cc824efa3bc52a10c4c2d6014e1e96b6d1226c7
pryan <pryan@EC2AMAZ-TALBF09.health.local>
2023-01-11 01:16:37 +0000

    fix nzbase copy

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -12,20 +12,20 @@ nzbase_version=$(yq '.dependencies."fhir.org.nz.ig.base".version' ./sushi-config
 echo nzbase url =$nzbase_url
 echo nzbase version =$nzbase_version
 
-
+#cp nzbase into user's .fhir cache 
+sudo mkdir temp
+cd temp
 wget -e use_proxy=yes -e https_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"package.tgz"
-mv ./package.tgz site/package.tgz
-
-tar zxvf ./site/package.tgz
-#fix the package url:
+tar zxvf package.tgz
+##fix the package url:
 jq --arg url $nzbase_url '.url |= $url' ./package/package.json > temp.json
 mv temp.json ./package/package.json 
-
 
 #cp nz packages  into user's .fhir cache 
 sudo mkdir -p  ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version/package
 sudo  cp -r ./package ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version
-
+cd ..
+.
 
 #cp hl7 packages into user's .fhir cache 
 aws s3 cp s3://nz-govt-moh-hip-build/codebuild-common/fhir/hl7.fhir.r4.core#4.0.1/package.zip ./hl7-package.zip
@@ -40,7 +40,7 @@ common_version=$(yq '.dependencies."hl7.org.nz.fhir.ig.hip-core".version' ./sush
 sudo mkdir ~/.fhir/packages//hl7.org.nz.fhir.ig.hip-core#$common_version
 ls -l ./hfc_package/hip-fhir-common*/package/package.tgz
 tar zxvf  ./hfc_package/hip-fhir-common*/package/package.tgz -C  ~/.fhir/packages/hl7.org.nz.fhir.ig.hip-core#$common_version
-#fix the package url:
+##fix the package url:
 jq --arg url $common_url '.url |= $url' ~/.fhir/packages/hl7.org.nz.fhir.ig.hip-core#$common_version/package/package.json > temp2.json
 mv temp2.json  ~/.fhir/packages/hl7.org.nz.fhir.ig.hip-core#$common_version/package/package.json
 
