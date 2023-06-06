9860779bd373d4d6c4022caffb255018624c5837
pryan <pryan@EC2AMAZ-TALBF09.health.local>
2023-06-06 22:24:16 +0000

    fixed validation

--- fhirValidate.sh
+++ fhirValidate.sh
@@ -7,8 +7,10 @@ cd ./output
 rm -r ./temp
 mkdir ./temp
 
+unzip ./full-ig.zip site/package.tgz
+
 echo validating Patient examples
 cp ./Patient-*.json ./temp
-java -jar ~/validator_cli.jar ./temp  -ig ./package.tgz -profile http://hl7.org.nz/fhir/StructureDefinition/NhiPatient -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -html-output ./validation_NhiPatient.html
+java -jar ~/validator_cli.jar ./temp  -ig ./site/package.tgz -profile http://hl7.org.nz/fhir/StructureDefinition/NhiPatient -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -html-output ./validation_NhiPatient.html
 #rm ./*temp/*.json
 
