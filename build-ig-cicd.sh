4b925ba448b60fc308a8f54dde3b4a566eaaad77
pryan <pat.ryan@health.govt.nz>
2024-07-01 03:16:34 +0000

    fix exit

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -131,7 +131,7 @@ chmod +x ./openapi/makeoas.sh
 echo running ig publisher
 java -jar ~/publisher.jar -ig . -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -no-sushi
 
-ls ./output-full-ig.sh
+ls ./output/full-ig.sh
 
 chmod +x ./fhirValidate.sh
 ./fhirValidate.sh
